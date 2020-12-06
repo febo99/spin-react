669c2e803b13d0df37541b33de82faca3d4633b2
Žiga Lah <ziga.lah999@gmail.com>
2020-12-06 09:13:13 +0100

    * Added map icon * Better date format

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import './App.css';
 import Tile from './components/Tile/Tile';
 import { Component } from 'react';
 
-
+const formatDate = (data) =>{
+  data.forEach((item) =>{
+    item.nastanekCas = (new Date(item.nastanekCas).toLocaleString("sl-SL").replace(". ", ".").replace(". ", "."));
+    item.map = 'https://www.google.com/maps/place/' + item.wgsLat + ',' + item.wgsLon ;
+  })
+  return data;
+}
 class App extends Component{
   state ={
     intervetions : []
@@ -11,7 +17,8 @@ class App extends Component{
     const api = 'https://spin3.sos112.si/javno/assets/data/lokacija.json';
     fetch(api).then((response) => response.json())
     .then(d => {
-        this.setState({ intervetions: d.value });
+        const finalItems = formatDate(d.value);
+        this.setState({ intervetions: finalItems });
     });
   }
   render(){
@@ -21,7 +28,11 @@ class App extends Component{
           <h1 id="App-h1">SPIN 112</h1>
           <div className="App-content">
             {this.state.intervetions.map((d) => {
-              return <Tile type={d.intervencijaVrstaNaziv} time={d.prijavaCas} location={d.obcinaNaziv} content={d.besedilo}></Tile>
+              return <Tile type={d.intervencijaVrstaNaziv} 
+              time={d.nastanekCas} 
+              location={d.obcinaNaziv}
+              content={d.besedilo}
+              mapLink={d.map}></Tile>
             })}
           </div>
         </header>
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import styles from './Tile.module.css';
 import PropTypes from 'prop-types';
+import Map from '../../map.svg';
 
 class Tile extends React.Component{
   render(){
     return(
     <div className={styles.Tile}>
       <div className={styles.TileHeader}>
-        <h1 id={styles.TileType}>{this.props.type}</h1>
-        <h1 id={styles.TileTime}>{this.props.time}</h1>
-        <h2 id={styles.TileLocation}>{this.props.location}</h2>
+        <div className={styles.TileHeaderText}>
+          <h1 id={styles.TileType}>{this.props.type}</h1>
+          <h1 id={styles.TileTime}>{this.props.time}</h1>
+          <h2 id={styles.TileLocation}>{this.props.location}</h2>
+        </div>
+        <div className={styles.TileMapIcon}>
+          <a target='_blank' rel='noreferrer' href={this.props.mapLink}><img alt='Map' src={Map}></img></a>
       </div>
+      </div>
+
       <div className={styles.TileContent}>
         <p>
         {this.props.content}
@@ -24,7 +31,8 @@ Tile.propTypes = {
   type: PropTypes.string,
   time: PropTypes.string,
   location: PropTypes.string,
-  content: PropTypes.string
+  content: PropTypes.string,
+  mapLink: PropTypes.string
 };
 
 Tile.defaultProps = {};
